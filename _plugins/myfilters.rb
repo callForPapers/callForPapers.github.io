ff401e7ef9d0faf45a4f5285e41617c02ecea34e
Piotr Stapp <ptrstpp950@gmail.com>
2015-11-13 23:31:22 +0100

    Fix file_date filter for RSS feed

--- _plugins/myfilters.rb
+++ _plugins/myfilters.rb
@@ -3,8 +3,18 @@ require 'jekyll-last-modified-at'
 module Jekyll
   module MyFilters
     def file_date(input)
-      File.mtime(input)
+       last_commit_date = Jekyll::LastModifiedAt::Executor.sh(
+            'git',
+            'log',
+            '-1',
+            '--format="%ct"',
+            '--',
+            input
+          )[/\d+/]
+          # last_commit_date can be nil iff the file was not committed.
+        (last_commit_date.nil? || last_commit_date.empty?) ? File::mtime(input) : last_commit_date
     end
+
     def next(input)
     	Jekyll::LastModifiedAt::Determinator.new(input, input)
     end
