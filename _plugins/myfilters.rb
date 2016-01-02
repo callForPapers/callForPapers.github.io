b538cfaf5066bdf185c16a1538582c3b35cd39e8
Piotr Stapp <prstpp950@gmail.com>
2016-01-02 13:27:43 +0100

    feed.xml - small changes hope to fix

--- _plugins/myfilters.rb
+++ _plugins/myfilters.rb
@@ -6,12 +6,12 @@ module Jekyll
     class Generator < Jekyll::Generator
       def generate(site)
         cfps = site.data['conferences'].map do |key,data|
-          path = "/_data/conferences/#{key}.json"
+          #path = "/_data/conferences/#{key}.json"
           #last_modified_at = Jekyll::LastModifiedAt::Determinator.new(site.source, path)
-          data['last_modified_at'] = path #last_modified_at.last_modified_at_time
+          #data['last_modified_at'] = last_modified_at.last_modified_at_time
           data
         end
-        site.data['cfps'] = cfps.sort_by!{|x| x['last_modified_at']}
+        site.data['cfps'] = cfps #cfps.sort_by!{|x| x['last_modified_at']}
       end
     end
   end
